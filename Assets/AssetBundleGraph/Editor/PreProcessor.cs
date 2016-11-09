15db1d3efc3a1c1bd5e97f505100908510778d08
Juan Legaz SP <juan.legaz@socialpoint.es>
2016-11-09 12:41:53 +0100

    Fix - added try-catch to preprocessor so progress bar is always cleared

--- Assets/AssetBundleGraph/Editor/PreProcessor.cs
+++ Assets/AssetBundleGraph/Editor/PreProcessor.cs
@@ -35,52 +35,57 @@ public class PreProcessor : AssetPostprocessor {
 		if(asset == null) { // This means it is the first time importing this asset.
 			var loader = LoaderSaveData.GetBestLoaderData(assetPath);
 			if(loader != null && loader.isPreProcess) {
-				var currentCount = 0.00f;
-				var totalCount = FullSaveData.Nodes.Count * 1f;
-				Action<NodeData, float> updateHandler = (node, progress) => {
-					var progressPercentage = ((currentCount / totalCount) * 100).ToString();
-					if(progressPercentage.Contains(".")) progressPercentage = progressPercentage.Split('.')[0];
+				try {
+					var currentCount = 0.00f;
+					var totalCount = FullSaveData.Nodes.Count * 1f;
+					Action<NodeData, float> updateHandler = (node, progress) => {
+						var progressPercentage = ((currentCount / totalCount) * 100).ToString();
+						if(progressPercentage.Contains(".")) progressPercentage = progressPercentage.Split('.')[0];
 
-					if(0 < progress) {
-						currentCount = currentCount + 1f;
+						if(0 < progress) {
+							currentCount = currentCount + 1f;
+						}
+
+						EditorUtility.DisplayProgressBar("AssetBundleGraph Processing... ", "Processing " + node.Name + ": " + progressPercentage + "%", currentCount / totalCount);
+					};
+					List<NodeException> errors = new List<NodeException>();
+					Action<NodeException> errorHandler = (NodeException e) => {
+						errors.Add(e);
+					};
+
+					var target = EditorUserBuildSettings.activeBuildTarget;
+
+
+					// perform setup. Fails if any exception raises.
+					AssetBundleGraphController.Perform(FullSaveData, target, false, errorHandler, null);
+
+					// if there is not error reported, then run
+					if(errors.Count == 0) {
+						var fakeLoaders = new Dictionary<string, List<string>>();
+						foreach(var node in FullSaveData.CollectAllNodes(x => x.Kind == NodeKind.LOADER_GUI && x.Id != loader.id)) {
+							fakeLoaders.Add(node.Id, new List<string>());
+						}
+						var assetPathList = new List<string>();
+						var absPath = Path.GetFullPath(assetPath);
+
+						if(Path.DirectorySeparatorChar != AssetBundleGraphSettings.UNITY_FOLDER_SEPARATOR) {
+							absPath = absPath.Replace(Path.DirectorySeparatorChar.ToString(), AssetBundleGraphSettings.UNITY_FOLDER_SEPARATOR.ToString());
+						}
+						assetPathList.Add(absPath);
+
+						fakeLoaders.Add(loader.id, assetPathList);
+						// run datas.                
+						AssetBundleGraphController.Perform(FullSaveData, target, true, errorHandler, updateHandler, fakeLoaders, assetImporter);
 					}
 
-					EditorUtility.DisplayProgressBar("AssetBundleGraph Processing... ", "Processing " + node.Name + ": " + progressPercentage + "%", currentCount / totalCount);
-				};
-				List<NodeException> errors = new List<NodeException>();
-				Action<NodeException> errorHandler = (NodeException e) => {
-					errors.Add(e);
-				};
-
-				var target = EditorUserBuildSettings.activeBuildTarget;
-
-				// perform setup. Fails if any exception raises.
-				AssetBundleGraphController.Perform(FullSaveData, target, false, errorHandler, null);
-
-				// if there is not error reported, then run
-				if(errors.Count == 0) {
-					var fakeLoaders = new Dictionary<string, List<string>>();
-					foreach(var node in FullSaveData.CollectAllNodes(x => x.Kind == NodeKind.LOADER_GUI && x.Id != loader.id)) {
-						fakeLoaders.Add(node.Id, new List<string>());
+					if(errors.Count > 0) {
+						Debug.LogError(errors[0]);
 					}
-					var assetPathList = new List<string>();
-					var absPath = Path.GetFullPath(assetPath);
-
-					if(Path.DirectorySeparatorChar != AssetBundleGraphSettings.UNITY_FOLDER_SEPARATOR) {
-						absPath = absPath.Replace(Path.DirectorySeparatorChar.ToString(), AssetBundleGraphSettings.UNITY_FOLDER_SEPARATOR.ToString());
-					}
-					assetPathList.Add(absPath);
-
-					fakeLoaders.Add(loader.id, assetPathList);
-					// run datas.                
-					AssetBundleGraphController.Perform(FullSaveData, target, true, errorHandler, updateHandler, fakeLoaders, assetImporter);
+				} catch(Exception e) {
+					Debug.LogError(e);
+				} finally {
+					EditorUtility.ClearProgressBar();
 				}
-
-				if(errors.Count > 0) {
-					Debug.LogError(errors[0]);
-				}
-
-				EditorUtility.ClearProgressBar();
 			}
 		}
 	}
